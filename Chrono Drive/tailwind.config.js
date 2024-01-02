65f10cdb70e61fc5386b717d195984f2313ae104
Hamadn <nhamad919@gmail.com>
2024-01-02 20:22:10 +0400

    Configure material tailwind for the website

--- Chrono Drive/tailwind.config.js
+++ Chrono Drive/tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern":
+          "url('/home/hamad/Chrono Drive Website/Chrono Drive/src/assets/Garage Background Wallpapers for Computer GsFDcY.jpg')",
+      },
+    },
   },
   plugins: [],
-};
+});
